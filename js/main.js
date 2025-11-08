bec73b8ab9a4f8227d8ad9dc8cb6980a716a7912
vivek gupta <131643827+Vivekgpta@users.noreply.github.com>
2025-11-08 20:45:53 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// For future interactive features
+console.log("College Memories website loaded");
+
+// Example: Add a click alert on cards
+document.querySelectorAll('.card').forEach(card => {
+    card.addEventListener('click', () => {
+        alert('You clicked on ' + card.querySelector('h3').innerText);
+    });
+});
