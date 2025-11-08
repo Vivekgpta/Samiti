0c0219ec4458f30bf10d04c189801711e9f06b8b
Vivekgpta <131643827+Vivekgpta@users.noreply.github.com>
2025-11-08 23:22:03 +0530

    this is new 1.0 update version

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@
 console.log("College Memories website loaded");
 
 // Example: Add a click alert on cards
-document.querySelectorAll('.card').forEach(card => {
-    card.addEventListener('click', () => {
-        alert('You clicked on ' + card.querySelector('h3').innerText);
-    });
-});
+// document.querySelectorAll('.card').forEach(card => {
+//     card.addEventListener('click', () => {
+//         alert('You clicked on ' + card.querySelector('h3').innerText);
+//     });
+// });
